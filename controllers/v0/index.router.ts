4e6499d90e121cfaac0cf6e11ab056c7616cda26
Flaviano Flauber <fmarinho@tce.pb.gov.br>
2022-11-22 14:45:55 -0300

    change in /users

--- controllers/v0/index.router.ts
+++ controllers/v0/index.router.ts
@@ -6,7 +6,7 @@ const router: Router = Router();
 router.use("/users", UserRouter);
 
 router.get("/", async (req: Request, res: Response) => {
-  res.send(`V0`);
+  res.send(`accessing /users`);
 });
 
 export const IndexRouter: Router = router;
