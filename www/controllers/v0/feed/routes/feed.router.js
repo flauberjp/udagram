4a0d2e866fb811ebcc6a861b6e5869a096f9dec1
Flaviano Flauber <fmarinho@tce.pb.gov.br>
2022-12-14 09:58:58 -0300

    add requireAuth to 'get all feed items' endpoint

--- www/controllers/v0/feed/routes/feed.router.js
+++ www/controllers/v0/feed/routes/feed.router.js
@@ -39,8 +39,8 @@ const auth_router_1 = require("../../users/routes/auth.router");
 const AWS = __importStar(require("../../../../aws"));
 const router = (0, express_1.Router)();
 // Get all feed items
-router.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const items = yield FeedItem_1.FeedItem.findAndCountAll({ order: [['id', 'DESC']] });
+router.get("/", auth_router_1.requireAuth, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const items = yield FeedItem_1.FeedItem.findAndCountAll({ order: [["id", "DESC"]] });
     items.rows.map((item) => {
         if (item.url) {
             item.url = AWS.getGetSignedUrl(item.url);
@@ -51,33 +51,35 @@ router.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
 // update a specific resource
-router.patch('/:id', auth_router_1.requireAuth, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.patch("/:id", auth_router_1.requireAuth, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     //@TODO try it yourself
     res.status(500).send("not implemented");
 }));
 // Get a signed url to put a new item in the bucket
-router.get('/signed-url/:fileName', auth_router_1.requireAuth, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.get("/signed-url/:fileName", auth_router_1.requireAuth, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     let { fileName } = req.params;
     const url = AWS.getPutSignedUrl(fileName);
     res.status(201).send({ url: url });
 }));
-// Post meta data and the filename after a file is uploaded 
+// Post meta data and the filename after a file is uploaded
 // NOTE the file name is they key name in the s3 bucket.
 // body : {caption: string, fileName: string};
-router.post('/', auth_router_1.requireAuth, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.post("/", auth_router_1.requireAuth, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const caption = req.body.caption;
     const fileName = req.body.url;
     // check Caption is valid
     if (!caption) {
-        return res.status(400).send({ message: 'Caption is required or malformed' });
+        return res
+            .status(400)
+            .send({ message: "Caption is required or malformed" });
     }
     // check Filename is valid
     if (!fileName) {
-        return res.status(400).send({ message: 'File url is required' });
+        return res.status(400).send({ message: "File url is required" });
     }
     const item = yield new FeedItem_1.FeedItem({
         caption: caption,
-        url: fileName
+        url: fileName,
     });
     const saved_item = yield item.save();
     saved_item.url = AWS.getGetSignedUrl(saved_item.url);
